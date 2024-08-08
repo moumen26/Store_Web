2d6fc92cb83fe2a055ea6117ebe8166da88dc98b
moumen26 <moumenkhaldi26@gmail.com>
2024-08-08 23:35:34 +0100

    Calendar Order Archive

--- src/pages/OrdersArchive.jsx
+++ src/pages/OrdersArchive.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import ButtonExportExel from "../components/ButtonExportExel";
 import OrdersArchiveTable from "../components/OrdersArchiveTable";
+import DashboardCalendar from "../components/DashboardCalendar";
 export default function OrdersArchive() {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
@@ -10,11 +11,33 @@ export default function OrdersArchive() {
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
   };
+  const [dateRange, setDateRange] = useState({
+    startDate: null,
+    endDate: null,
+  });
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      // Update dashboard content based on the selected date range
+      updateDashboardContent(dateRange.startDate, dateRange.endDate);
+    }
+  }, [dateRange]);
+
+  const updateDashboardContent = (startDate, endDate) => {
+    // Logic to update dashboard content based on selected date range
+    console.log("Selected range:", startDate, endDate);
+    // Fetch or filter data based on date range and update dashboard content
+  };
   return (
     <div className="pagesContainer">
       <Header />
       <div className="w-full flex items-center justify-between">
         <h2 className="pagesTitle">Orders Archive</h2>
+        <DashboardCalendar
+          onDateChange={(start, end) =>
+            setDateRange({ startDate: start, endDate: end })
+          }
+        />
       </div>
       <div className="pageTable">
         <div className="w-full flex items-center justify-between">

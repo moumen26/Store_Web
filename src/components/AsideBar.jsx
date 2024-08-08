debddb01794173d8861619a46ecfafd47a7840ed
moumen26 <moumenkhaldi26@gmail.com>
2024-08-08 18:46:36 +0100

    Update AsideBar

--- src/components/AsideBar.jsx
+++ src/components/AsideBar.jsx
@@ -20,14 +20,20 @@ export default function Asidebar() {
   const location = useLocation();
 
   const [isProductsOpen, setIsProductsOpen] = useState(false);
+  const [isOrdersOpen, setIsOrdersOpen] = useState(false);
 
   const handleProductsClick = () => {
     setIsProductsOpen((prevState) => !prevState);
   };
 
+  const handleOrdersClick = () => {
+    setIsOrdersOpen((prevState) => !prevState);
+  };
+
   const handleClickOutside = (event) => {
     if (!event.target.closest(".sidebar")) {
       setIsProductsOpen(false);
+      setIsOrdersOpen(false);
     }
   };
 
@@ -35,14 +41,16 @@ export default function Asidebar() {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
+
   const { logout } = useLogout();
-  //handle logout
+
   const submitLogout = () => {
     logout();
   };
+
   return (
     <aside>
-      <ul className="flex-col space-y-7">
+      <ul className="flex-col space-y-[18px]">
         <li>
           <NavLink to="/Dashboard" className="flex items-center">
             <div
@@ -55,12 +63,12 @@ export default function Asidebar() {
             </div>
           </NavLink>
         </li>
-        <div className="flex-col space-y-7 sidebar">
-          <li className="flex-col space-y-7">
+        <div className="flex-col space-y-[18px] sidebar">
+          <li className="flex-col space-y-[18px]">
             <div className="flex items-center cursor-pointer">
               <div
                 className={`flex items-center justify-between itemAsideBar ${
-                  location.pathname === "/Products" ? "asideItemActive" : ""
+                  location.pathname === "/Products" ? "" : ""
                 }`}
                 onClick={handleProductsClick}
               >
@@ -74,7 +82,7 @@ export default function Asidebar() {
               </div>
             </div>
             {isProductsOpen && (
-              <div className="flex-col space-y-7">
+              <div className="flex-col space-y-[18px]">
                 <NavLink to="/ProductsList" className="flex items-center">
                   <div
                     className={`flex items-center itemAsideBar ${
@@ -103,18 +111,52 @@ export default function Asidebar() {
             )}
           </li>
         </div>
-        <li>
-          <NavLink to="/Orders" className=" flex items-center">
-            <div
-              className={`flex items-center itemAsideBar ${
-                location.pathname === "/Orders" ? "asideItemActive" : ""
-              }`}
-            >
-              <ShoppingBagIcon className="iconAsideBar" />
-              <span className="ml-3">Orders</span>
+        <div className="flex-col space-y-[18px] sidebar">
+          <li className="flex-col space-y-[18px]">
+            <div className="flex items-center cursor-pointer">
+              <div
+                className={`flex items-center justify-between itemAsideBar ${
+                  location.pathname === "/Orders" ? "" : ""
+                }`}
+                onClick={handleOrdersClick}
+              >
+                <div className="flex">
+                  <ShoppingBagIcon className="iconAsideBar" />
+                  <span className="ml-3">Orders</span>
+                </div>
+                <ChevronDownIcon
+                  className={`iconPages ${isOrdersOpen ? "rotate-180" : ""}`}
+                />
+              </div>
             </div>
-          </NavLink>
-        </li>
+            {isOrdersOpen && (
+              <div className="flex-col space-y-[18px]">
+                <NavLink to="/Orders" className="flex items-center">
+                  <div
+                    className={`flex items-center itemAsideBar ${
+                      location.pathname === "/Orders" ? "asideItemActive" : ""
+                    }`}
+                  >
+                    <Square2StackIcon className="iconAsideBar opacity-0" />
+                    <span className="ml-3">Latest Orders</span>
+                  </div>
+                </NavLink>
+                <NavLink to="/OrdersGrid" className="flex items-center">
+                  <div
+                    className={`flex items-center itemAsideBar ${
+                      location.pathname === "/OrdersGrid"
+                        ? "asideItemActive"
+                        : ""
+                    }`}
+                  >
+                    <Square2StackIcon className="iconAsideBar opacity-0" />
+                    <span className="ml-3">Orders Archive</span>
+                  </div>
+                </NavLink>
+              </div>
+            )}
+          </li>
+        </div>
         <li>
           <NavLink to="/Vendors" className=" flex items-center">
             <div
@@ -175,7 +217,7 @@ export default function Asidebar() {
             </div>
           </NavLink>
         </li>
-        <div className="absolute bottom-0 space-y-7">
+        <div className="absolute bottom-0 space-y-[18px] w-full">
           <li>
             <NavLink to="/Settings" className=" flex items-center">
               <div

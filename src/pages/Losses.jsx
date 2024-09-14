1882e0b88a03a540c0f3b1ec4907741313333756
moumen26 <moumenkhaldi26@gmail.com>
2024-09-14 22:12:53 +0100

    Add new Loss Modal

--- src/pages/Losses.jsx
+++ src/pages/Losses.jsx
@@ -5,6 +5,11 @@ import ButtonExportExel from "../components/ButtonExportExel";
 import DashboardCalendar from "../components/DashboardCalendar";
 import OrderCard from "../components/OrderCard";
 import LossesTable from "../components/LossesTable";
+import ButtonAdd from "../components/ButtonAdd";
+import Modal from "react-modal";
+
+// Ensure you set the root element for accessibility
+Modal.setAppElement("#root");
 
 export default function Losses() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -32,6 +37,16 @@ export default function Losses() {
     // Fetch or filter data based on date range and update dashboard content
   };
 
+  const [openModelAddLoss, setOpenModelAddLoss] = useState(false);
+
+  const handleOpenModalAddLoss = () => {
+    setOpenModelAddLoss(true);
+  };
+
+  const handleCloseModalAddLoss = () => {
+    setOpenModelAddLoss(false);
+  };
+
   return (
     <div className="pagesContainer">
       <Header />
@@ -53,7 +68,84 @@ export default function Losses() {
             value={searchQuery}
             onChange={handleSearchChange}
           />
-          <ButtonExportExel data={filteredData} filename="Losses" />
+          <div className="flex space-x-2">
+            <ButtonExportExel data={filteredData} filename="Losses" />
+            <ButtonAdd
+              buttonSpan="Add a Loss"
+              onClick={handleOpenModalAddLoss}
+            />
+            <Modal
+              isOpen={openModelAddLoss}
+              onRequestClose={handleCloseModalAddLoss}
+              contentLabel="Add Address Modal"
+              style={{
+                overlay: {
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  zIndex: 1000,
+                },
+                content: {
+                  border: "none",
+                  borderRadius: "8px",
+                  padding: "20px",
+                  maxWidth: "40%",
+                  margin: "auto",
+                  height: "fit-content",
+                  zIndex: 1001,
+                },
+              }}
+            >
+              <div className="customerClasss">
+                <h2 className="customerClassTitle">Add New Loss</h2>
+                <div className="flex-col space-y-4">
+                  <div className="flex justify-end items-center space-x-4">
+                    <span>Loss :</span>
+                    <div className="inputForm pl-0">
+                      <input
+                        type="text"
+                        name="newLoss"
+                        // onChange={}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex justify-end items-center space-x-4">
+                    <span>Amount :</span>
+                    <div className="inputForm pl-0">
+                      <input
+                        type="text"
+                        name="lossAmount"
+                        // onChange={}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex justify-end items-center space-x-4">
+                    <span>Cause :</span>
+                    <div className="inputForm pl-0">
+                      <input
+                        type="text"
+                        name="lossCause"
+                        // onChange={}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex justify-end space-x-8 items-start mt-[20px]">
+                  <button
+                    className="text-gray-500 cursor-pointer hover:text-gray-700"
+                    onClick={handleCloseModalAddLoss}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="text-blue-500 cursor-pointer hover:text-blue-700"
+                    // onClick={handleAddItem}
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          </div>
         </div>
         <div className="pageTableContainer">
           <LossesTable

d268ec8fa104c01dec3a1fc3b2a80b79437e8c33
ZINE EDDINE <119061162+ZYNOCODES@users.noreply.github.com>
2024-10-11 17:12:48 +0100

    publicity API integration commit

--- src/components/PubSwiperStore.jsx
+++ src/components/PubSwiperStore.jsx
@@ -1,51 +1,72 @@
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-
-import Pub1 from "../assets/Photos/Pub1.png";
-import Pub2 from "../assets/Photos/Pub2.png";
-
-// import required modules
 import { FreeMode, Pagination } from "swiper/modules";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import ConfirmDialog from "./ConfirmDialog";
+import axios from "axios";
+import { TokenDecoder } from "../util/DecodeToken";
+import { Alert, CircularProgress, Snackbar } from "@mui/material";
 
-const DataPub = [
-  {
-    id: 1,
-    name: "Pub 1",
-    image: Pub1,
-  },
-  {
-    id: 2,
-    name: "Pub 2",
-    image: Pub2,
-  },
-  {
-    id: 2,
-    name: "Pub 2",
-    image: Pub2,
-  },
-  {
-    id: 2,
-    name: "Pub 2",
-    image: Pub2,
-  },
-];
-
-export default function PubSwiperStore() {
+export default function PubSwiperStore({data, loading, StorePublicityRefetch, user}) {
+  const decodedToken = TokenDecoder();
   const [DeletePub, setDeletePub] = useState(false);
-  const handleOpenDeletePub = () => {
+  const [deletedPublicity, setdeletedPublicity] = useState(null);
+  const handleOpenDeletePub = (id) => {
     setDeletePub(true);
+    setdeletedPublicity(id);
   };
-  const handleCloseDeletePub = () => {
-    setDeletePub(false);
+  
+  const [submitionLoading, setSubmitionLoading] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [alertType, setAlertType] = useState(true);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const handleDeletePublicity = async () => {
+    try {
+      setSubmitionLoading(true);
+      const response = await axios.delete(
+        import.meta.env.VITE_APP_URL_BASE + `/Publicity/deleteFromStore/${deletedPublicity}/${decodedToken.id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.token}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        setAlertType(false);
+        setSnackbarMessage(response.data.message);
+        setSnackbarOpen(true);
+        StorePublicityRefetch();
+        setSubmitionLoading(false);
+        setDeletePub(false);
+        setdeletedPublicity(null);
+      } else {
+        setAlertType(true);
+        setSnackbarMessage(response.data.message);
+        setSnackbarOpen(true);
+        setSubmitionLoading(false);
+        setdeletedPublicity(null);
+      }
+    } catch (error) {
+      if (error.response) {
+        setAlertType(true);
+        setSnackbarMessage(error.response.data.message);
+        setSnackbarOpen(true);
+        setSubmitionLoading(false);
+      } else if (error.request) {
+        // Request was made but no response was received
+        console.error("Error deleting Publicity: No response received");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error deleting Publicity", error);
+      }
+    }
   };
+
   return (
     <>
       <Swiper
@@ -54,22 +75,59 @@ export default function PubSwiperStore() {
         modules={[FreeMode, Pagination]}
         className="pubSwiper"
       >
-        {DataPub.map((pub) => (
-          <SwiperSlide key={pub.id} className="swiperSlide">
-            <XMarkIcon
-              className="h-6 w-6 trashIcon text-red-500 cursor-pointer hover:text-red-700"
-              onClick={handleOpenDeletePub}
-            />
-            <img src={pub.image} alt={pub.name} />
-          </SwiperSlide>
-        ))}
+        {loading || submitionLoading ? (
+          <div className="w-full h-full flex items-center justify-center">
+            <CircularProgress color="inherit" />
+          </div>
+        ) : !Array.isArray(data) || data?.lenght <= 0 ? (
+          <div className="w-full h-full flex items-center justify-center">
+            <span>No publicity found</span>
+          </div>
+        ) : (data?.map((pub) => (
+            <SwiperSlide key={pub._id} className="swiperSlide">
+              <XMarkIcon
+                className="h-6 w-6 trashIcon text-red-500 cursor-pointer hover:text-red-700"
+                onClick={() => handleOpenDeletePub(pub._id)}
+              />
+              <img 
+                src={`${import.meta.env.VITE_APP_URL_BASE.replace(
+                  "/api",
+                  ""
+                )}/files/${pub.image}`} 
+                alt={pub.image}
+                style={{
+                  opacity: `${pub.distination == 'public' && pub.displayPublic == false ? '0.5' : '1'}`,
+                }}
+              />
+            </SwiperSlide>
+          ))
+        )}
       </Swiper>
       <ConfirmDialog
         open={DeletePub}
-        onClose={handleCloseDeletePub}
+        onClose={()=> {
+          setDeletePub(false)
+          setdeletedPublicity(null)
+        }}
+        onConfirm={handleDeletePublicity}
         dialogTitle={"Confirm Publicité Deletion"}
         dialogContentText={`Are you sure you want to delete this publicité?`}
+        loading={submitionLoading}
       />
+      {/* Snackbar */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity={alertType ? "error" : "success"}
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </>
   );
 }
--- src/pages/Publicité.jsx
+++ src/pages/Publicité.jsx
@@ -6,58 +6,156 @@ import ButtonAdd from "../components/ButtonAdd";
 import Modal from "react-modal";
 import PublicitéTable from "../components/PublicitéTable";
 import PubSwiperStore from "../components/PubSwiperStore";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Alert, CircularProgress, Snackbar } from "@mui/material";
+import axios from "axios";
+import { TokenDecoder } from "../util/DecodeToken";
 
 // Ensure you set the root element for accessibility
 Modal.setAppElement("#root");
 
 export default function Publicité() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
-
-  const handleSearchChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const [dateRange, setDateRange] = useState({
-    startDate: null,
-    endDate: null,
-  });
-
-  useEffect(() => {
-    if (dateRange.startDate && dateRange.endDate) {
-      // Update dashboard content based on the selected date range
-      updateDashboardContent(dateRange.startDate, dateRange.endDate);
-    }
-  }, [dateRange]);
-
-  const updateDashboardContent = (startDate, endDate) => {
-    // Logic to update dashboard content based on selected date range
-    console.log("Selected range:", startDate, endDate);
-    // Fetch or filter data based on date range and update dashboard content
-  };
-
+  const decodedToken = TokenDecoder();
+  const { user } = useAuthContext();
+  const location = useLocation();
   const [openModelAddPub, setOpenModelAddPub] = useState(false);
-
+  const [submitionLoading, setSubmitionLoading] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [alertType, setAlertType] = useState(true);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [Distination , setDistination] = useState("");
+  const handleDistinationChange = (e) => {
+    setDistination(e.target.value);
+  };
   const handleOpenModalAddPub = () => {
     setOpenModelAddPub(true);
   };
 
-  const handleCloseModalAddLoss = () => {
+  const handleCloseModalAddPub = () => {
     setOpenModelAddPub(false);
   };
 
   const [uploadedImage, setUploadedImage] = useState(null);
-
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
-    if (file) {
-      setUploadedImage(URL.createObjectURL(file)); // Store the preview URL of the image
+    if (file && file.type.startsWith("image/")) {
+      setUploadedImage(file);
     }
   };
 
   const openFileInput = () => {
-    document.getElementById("imageUploadInput").click(); // Trigger the file input dialog
+    document.getElementById("imageUploadInput").click();
   };
+
+  const clearForm = () => {
+    setUploadedImage(null);
+  };
+
+  // fetching StorePublicity data
+  const fetchStorePublicityData = async () => {
+    const response = await fetch(
+      import.meta.env.VITE_APP_URL_BASE + `/Publicity/fetchAllStorePublicities/${decodedToken.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.token}`,
+        },
+      }
+    );
+
+    // Handle the error state
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (errorData.error.statusCode == 404) return [];
+      else throw new Error("Error receiving StorePublicity data");
+    }
+    // Return the data
+    return await response.json();
+  };
+  // useQuery hook to fetch data
+  const {
+    data: StorePublicityData,
+    error: StorePublicityError,
+    isLoading: StorePublicityLoading,
+    refetch: StorePublicityRefetch,
+  } = useQuery({
+    queryKey: ["StorePublicityData", user?.token, location.key],
+    queryFn: fetchStorePublicityData,
+    enabled: !!user?.token, 
+    refetchOnWindowFocus: true, 
+  });
+
+  //save Publicity API
+  const handleSavePublicity = async () => {
+    try {
+      setSubmitionLoading(true);
+      const formData = new FormData();
+      formData.append("file", uploadedImage);
+      formData.append("distination", Distination);
+
+      const response = await axios.post(
+        import.meta.env.VITE_APP_URL_BASE + `/Publicity/createFromStore/${decodedToken.id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${user?.token}`,
+          },
+        }
+      );
+      if (response.status === 200) {
+        setAlertType(false);
+        setSnackbarMessage(response.data.message);
+        setSnackbarOpen(true);
+        StorePublicityRefetch();
+        setSubmitionLoading(false);
+        clearForm();
+        handleCloseModalAddPub();
+      } else {
+        setAlertType(true);
+        setSnackbarMessage(response.data.message);
+        setSnackbarOpen(true);
+        setSubmitionLoading(false);
+      }
+    } catch (error) {
+      if (error.response) {
+        setAlertType(true);
+        setSnackbarMessage(error.response.data.message);
+        setSnackbarOpen(true);
+        setSubmitionLoading(false);
+      } else if (error.request) {
+        // Request was made but no response was received
+        console.error("Error creating Publicity: No response received");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error("Error creating Publicity", error);
+      }
+    }
+  };
+
+  if (StorePublicityLoading) {
+    return (
+      <div className="pagesContainer h-[100vh]">
+        <Header />
+        <div className="w-full h-full flex items-center justify-center">
+          <CircularProgress color="inherit" />
+        </div>
+      </div>
+    );
+  }
+  if (StorePublicityError) {
+    return (
+      <div className="pagesContainer">
+        <Header />
+        <div className="customerClass">
+          <h2 className="customerClassTitle">no data is available</h2>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="pagesContainer pubContainer">
@@ -68,77 +166,112 @@ export default function Publicité() {
           buttonSpan="Add a Publicité"
           onClick={handleOpenModalAddPub}
         />
-        <Modal
-          isOpen={openModelAddPub}
-          onRequestClose={handleCloseModalAddLoss}
-          contentLabel="Add new Publicité"
-          style={{
-            overlay: {
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-              zIndex: 1000,
-            },
-            content: {
-              border: "none",
-              borderRadius: "8px",
-              padding: "20px",
-              maxWidth: "40%",
-              margin: "auto",
-              height: "fit-content",
-              zIndex: 1001,
-            },
-          }}
-        >
-          <div className="customerClasss">
-            <h2 className="customerClassTitle">Add New Publicité</h2>
+      </div>
+      <PubSwiperStore 
+        data={StorePublicityData}
+        loading={StorePublicityLoading}
+        StorePublicityRefetch={StorePublicityRefetch}
+        user={user}
+      />
+      <Modal
+        isOpen={openModelAddPub}
+        onRequestClose={handleCloseModalAddPub}
+        contentLabel="Add new Publicité"
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            zIndex: 1000,
+          },
+          content: {
+            border: "none",
+            borderRadius: "8px",
+            padding: "20px",
+            maxWidth: "40%",
+            margin: "auto",
+            height: "fit-content",
+            zIndex: 1001,
+          },
+        }}
+      >
+        <div className="customerClasss">
+          <h2 className="customerClassTitle">Add New Publicité</h2>
 
-            {/* Image Upload Section */}
-            <div className="mt-[20px]">
-              <div
-                className="w-full h-[300px] flex justify-center items-center border-dashed border-[2px] border-gray-400 cursor-pointer"
-                onClick={openFileInput}
-              >
-                {uploadedImage ? (
-                  <img
-                    src={uploadedImage}
-                    alt="Uploaded"
-                    className="w-full h-full object-contain"
-                  />
-                ) : (
-                  <p className="uploadSpan">
-                    <span className="text-blue-600">Click to upload </span>
-                    or drag and drop SVG, PNG, JPG
-                  </p>
-                )}
-
-                <input
-                  id="imageUploadInput"
-                  type="file"
-                  style={{ display: "none" }}
-                  onChange={handleImageUpload}
+          {/* Image Upload Section */}
+          <div className="mt-[20px]">
+            <div
+              className="w-full h-[300px] flex justify-center items-center border-dashed border-[2px] border-gray-400 cursor-pointer"
+              onClick={openFileInput}
+            >
+              {uploadedImage ? (
+                <img
+                  src={URL.createObjectURL(uploadedImage)}
+                  alt="Uploaded"
+                  className="w-full h-full object-contain"
                 />
-              </div>
-            </div>
+              ) : (
+                <p className="uploadSpan">
+                  <span className="text-blue-600">Click to upload </span>
+                  or drag and drop SVG, PNG, JPG
+                </p>
+              )}
 
-            {/* Save and Cancel Buttons */}
-            <div className="flex justify-end space-x-8 items-start mt-[20px]">
-              <button
-                className="text-gray-500 cursor-pointer hover:text-gray-700"
-                onClick={handleCloseModalAddLoss}
-              >
-                Cancel
-              </button>
-              <button
-                className="text-blue-500 cursor-pointer hover:text-blue-700"
-                // Add your logic to handle saving the image or data here
-              >
-                Save
-              </button>
+              <input
+                id="imageUploadInput"
+                type="file"
+                style={{ display: "none" }}
+                onChange={handleImageUpload}
+              />
             </div>
           </div>
-        </Modal>
-      </div>
 
-      <PubSwiperStore />
+          {/* Distination Input select */}
+          <div className="flex space-x-5 items-center">
+            <span>Distination :</span>
+            <div className="selectStoreWilayaCommune w-[300px]">
+              <select
+                name="productCategory"
+                onChange={handleDistinationChange}
+              >
+                <option value="">
+                  -- Select Distination --
+                </option>
+                <option value="private">private</option>
+                <option value="public">Public</option>
+              </select>
+            </div>
+          </div>
+
+          {/* Save and Cancel Buttons */}
+          <div className="flex justify-end space-x-8 items-start mt-[20px]">
+            <button
+              className="text-gray-500 cursor-pointer hover:text-gray-700"
+              onClick={handleCloseModalAddPub}
+            >
+              Cancel
+            </button>
+            <button
+              className="text-blue-500 cursor-pointer hover:text-blue-700"
+              onClick={handleSavePublicity}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </Modal>
+      {/* Snackbar */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity={alertType ? "error" : "success"}
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
